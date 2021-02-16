b2c1818a768017ba41d686d67900647b8c2e3c4e
m-schm <55513173+m-schm@users.noreply.github.com>
2021-02-16 06:15:39 -0800

    two-phase scope-checking

--- src/Language/Topaz/ScopeCheck.hs
+++ src/Language/Topaz/ScopeCheck.hs
@@ -1,8 +1,12 @@
-module Language.Topaz.ScopeCheck where
+module Language.Topaz.ScopeCheck (scopeCheck) where
 import Relude
 import Language.Topaz.Types.AST
 
-data instance TTGIdent 'ScopeCheck = LocalDef Ident | Known ModulePath Ident
+data instance TTGIdent 'ScopeCheck
+  = DuringCheck (TTGIdent 'ScopeChecked)
+  | Unknown (Maybe ModulePath) Ident
+  deriving Show
+data instance TTGIdent 'ScopeChecked = LocalDef Ident | Known ModulePath Ident
   deriving Show
 
 data Env = Env
@@ -10,3 +14,28 @@ data Env = Env
   , qualified ∷ Map ModulePath (Set Ident)
   , locals ∷ [Set Ident]
   }
+
+newtype ChkM a = ChkM (State Env a)
+  deriving newtype (Functor, Applicative, Monad)
+
+data ScopeError
+  = NoIdent (Maybe ModulePath) Ident
+  | NoQual ModulePath
+
+scopeCheck ∷ TopLevel 'Parse
+  → Either (NonEmpty ScopeError) (TopLevel 'ScopeChecked)
+scopeCheck = undefined
+
+phase1 ∷ TopLevel 'Parse → ChkM (TopLevel 'ScopeCheck)
+phase1 (TopLevel mp ds me) = TopLevel mp
+  <$> traverse declTop ds
+  <*> traverse expr me
+
+declTop ∷ Decl 'Parse TopLevel → ChkM (Decl 'ScopeCheck TopLevel)
+declTop = error "not implemented"
+
+decl ∷ Decl 'Parse Block → ChkM (Decl 'ScopeCheck Block)
+decl = error "not implemented"
+
+expr ∷ Expr 'Parse → ChkM (Expr 'ScopeCheck)
+expr = error "not implemented"
