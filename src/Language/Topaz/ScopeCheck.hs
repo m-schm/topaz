ef0357263faa0d42b206f7f6b25bae51f0bd03d9
m-schm <55513173+m-schm@users.noreply.github.com>
2021-02-16 22:36:01 -0800

    local names are also unqualified, merge them

--- src/Language/Topaz/ScopeCheck.hs
+++ src/Language/Topaz/ScopeCheck.hs
@@ -3,7 +3,7 @@ module Language.Topaz.ScopeCheck
   ) where
 import Relude hiding (local)
 import Control.Lens
-import Language.Topaz.Types.AST
+import Language.Topaz.Types.AST hiding (Local)
 import Language.Topaz.Desugar ()
 
 import qualified Data.List.NonEmpty as NE
@@ -14,18 +14,19 @@ type instance TTGIdent 'ScopeChecked = KnownIdent
 type instance TTGArgs 'ScopeChecked = ()
 type instance TTGLam 'ScopeChecked = Loc (Arg 'ScopeChecked)
 
+data NameSource = Imported ModulePath | Local
+
 data Env = Env
-  { unqualified ∷ Map Ident ModulePath
+  { unqualified ∷ Map Ident NameSource
   , qualified ∷ Map ModulePath (Set Ident)
-  , locals ∷ Set Ident
   } deriving Generic
 
 instance Semigroup Env where
-  Env uq q l <> Env uq' q' l' = Env (uq <> uq') (q <> q') (l <> l')
+  Env uq q <> Env uq' q' = Env (uq <> uq') (q <> q')
 
 instance Monoid Env where
   mappend = (<>)
-  mempty = Env mempty mempty mempty
+  mempty = Env mempty mempty
 
 newtype ChkM a = ChkM (StateT (NonEmpty Env) Result a)
   deriving newtype (Functor, Applicative, Monad)
@@ -89,16 +90,16 @@ arg ∷ Arg 'Desugared → ChkM (Arg 'ScopeChecked)
 arg (Arg mi e) = do
   e' ← expr e
   whenJust mi \i →
-    #locals %= S.insert i
+    #unqualified . at i ?= Local
   pure $ Arg mi e'
 arg (Implicit i e) = do
   e' ← expr e
-  #locals %= S.insert i
+  #unqualified . at i ?= Local
   pure $ Implicit i e'
 arg (Instance mi e) = do
   e' ← expr e
   whenJust mi \i →
-    #locals %= S.insert i
+    #unqualified . at i ?= Local
   pure $ Arg mi e'
 
 local ∷ ChkM a → ChkM a
