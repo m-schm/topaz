4dfee237f056d99344abaacaab935d0246bfc8c7
m-schm <55513173+m-schm@users.noreply.github.com>
2021-06-03 10:08:36 -0700

    fix ICofree and add Show instance

--- src/Language/Topaz/Types/Indexed.hs
+++ src/Language/Topaz/Types/Indexed.hs
@@ -5,4 +5,9 @@ type IFix ∷ ((k → Type) → k → Type) → k → Type
 newtype IFix f i = IFix (f (IFix f) i)
 
 type ICofree ∷ Type → ((k → Type) → k → Type) → k → Type
-data ICofree a f i = a :@< f (IFix f) i
+data ICofree a f i = a :@< f (ICofree a f) i
+
+deriving instance
+  ( Show a
+  , ∀ r j. (∀ k. Show (r k)) ⇒ Show (f r j)
+  ) ⇒ Show (ICofree a f i)
